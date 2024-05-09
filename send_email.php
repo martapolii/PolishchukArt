b5abba18fa5b4f25ddfef4f9486e788fc8ad5617
martapolii <mpolish3@my.centennialcollege.ca>
2024-05-09 18:29:11 -0400

    Added a PHP script to contact form so form is sent out to clients email without leaving webpage. Also removed background image from contact form and simplified the styling, as per client request.

---
+++ send_email.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['guestName'];
+    $email = $_POST['guestEmail'];
+    $message = $_POST['message'];
+    
+    $to = 'm.polishchuk@hotmail.ca';
+    $subject = 'New Message from PolishchukArt Contact Form';
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+    
+    if (mail($to, $subject, $body)) {
+        echo '<script>alert("Your message has been sent successfully.");</script>';
+    } else {
+        echo '<script>alert("There was an error sending your message. Please try again later.");</script>';
+    }
+}
+
