31997aafd34122eafbc05141db80ca4106a1328b
marta <m.polishchuk@hotmail.ca>
2024-09-04 21:46:45 -0400

    Adjust button on hero image for large screen sizes.

---
+++ mobile_nav.js
@@ -0,0 +1,10 @@
+// want to turn the hamburger menu into an overlay of the entire screen (based on my research this is how menus are commonly displayed on mobile sites  - it looks a lot cleaner)
+
+function navOverlay()
+{
+//append elements I want to display in the overlay to the overlay:
+
+
+
+
+}
